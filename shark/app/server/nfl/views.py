5c3264ef97d3b27e823fa469099ddd2e15c4543c
Troy <tathomas@email.wm.edu>
2017-06-26 19:19:23 -0700

    add league and user models

--- shark/app/server/nfl/views.py
+++ shark/app/server/nfl/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from nfl.models import Team, Game
-from nfl.serializers import TeamSerializer, GameSerializer
+from nfl.models import Team, Game, League, User
+from nfl.serializers import TeamSerializer, GameSerializer, LeagueSerializer, UserSerializer
 
 class TeamViewSet(viewsets.ModelViewSet):
 	""" ViewSet for viewing and editing Team objects """
@@ -11,4 +11,14 @@ class GameViewSet(viewsets.ModelViewSet):
 	""" ViewSet for viewing and editing Game objects """
 	queryset = Game.objects.all()
 	serializer_class = GameSerializer
+
+class LeagueViewSet(viewsets.ModelViewSet):
+	""" Viewset for viewing and editing League objects """
+	queryset = League.objects.all()
+	serializer_class = LeagueSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+	""" Viewset for viewing and editing User objects """
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
 
